e09a22e5b1ef3d187d5c43cfa6a4116c1a36439b
quytelda <quytelda@ae574561-ced2-473d-8cd8-d847de7b6355>
2014-07-22 15:13:54 +0000

    added GPL stuff
    git-svn-id: http://tamalin.org/svn/greenircd@50 ae574561-ced2-473d-8cd8-d847de7b6355

--- irc/connection.py
+++ irc/connection.py
@@ -1,16 +1,31 @@
+# main.py - Generic container interface for peer connections
 #
-# irc/connection.py
+# Copyright (C) 2014 Quytelda Gaiwin <admin@tamalin.org>
 #
+# This file is part of GreenIRCd, the python IRC daemon.
+#
+# GreenIRCd is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# WeeChat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GreenIRCd.  If not, see <http://www.gnu.org/licenses/>.
 
 import socket
-import hashlib
 
 class IRCConnection:	
-	def __init__(self, server, ctcn):
+	def __init__(self, server, ctcn, primary = True):
 		self.ctcn = ctcn
 		self.server = server
 		
-		ctcn.container = self
+		if primary:
+			ctcn.container = self
 	
 	def handle_data(self, data):
 		"""Passes the server the data from the connection.  In the future, this message should ensure that this is (a) allowed, and (b) valid."""
--- symbols.py
+++ symbols.py
@@ -1,6 +1,21 @@
+# symbols.py - GreenIRCd symbol definitions
 #
-# symbols.py
+# Copyright (C) 2014 Quytelda Gaiwin <admin@tamalin.org>
 #
+# This file is part of GreenIRCd, the python IRC daemon.
+#
+# GreenIRCd is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# WeeChat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GreenIRCd.  If not, see <http://www.gnu.org/licenses/>.
 
 # TODO: chanmodes +pbki, usermodes +si
 
@@ -11,6 +26,7 @@ CHHOP = 2**2
 CHVOICE = 2**1
 CHUSER = 2**0
 
+# Numeric replies defined in the RFC for the IRC protocol
 RPL_WELCOME = 001
 RPL_YOURHOST = 002
 RPL_MYINFO = 004
@@ -48,6 +64,7 @@ ERR_ALREADYREGISTERED = 462 # :<reason>
 ERR_CHANNELISFULL = 471 # <channel> :<reason>
 ERR_NOPRIVILEGES = 481 # :<reason>
 
+# channel management status modes
 status_modes = {
 	CHUSER : {'prefix' : '', 'modechar' : ''},
 	CHVOICE : {'prefix' : '+', 'modechar' : 'v'},
@@ -57,27 +74,29 @@ status_modes = {
 	CHOWNER : {'prefix' : '~', 'modechar' : 'q'}
 }
 
-# lvhopsmntikrRcaqOALQbSeIKVfMCuzNTGjZ
-# ntslmPZOvhoaq
+# channel mode symbols and masks
 chan_modes = {
-	'n' : 2**0,
-	't' : 2**1,
-	's' : 2**2,
-	'l' : 2**3,
-	'm' : 2**4,
-	'P' : 2**5,
-	'Z' : 2**6,
-	'O' : 2**7,
-	'A' : 2**8,
+	'n' : 2**0, # no outside messages
+	't' : 2**1, # restricted topic changes
+	's' : 2**2, # secret channel (unlisted)
+	'l' : 2**3, # channel size limit (takes integer param)
+	'm' : 2**4, # moderated
+	'P' : 2**5, # persistant ops
+	'Z' : 2**6, # SSL users only
+	'O' : 2**7, # server operators only
+	'A' : 2**8, # administrators only
 
-	# status modes
-	'v' : 0,
-	'h' : 0,
-	'o' : 0,
-	'a' : 0,
-	'q' : 0
+	# channel status modes
+	# since these modes affect the status of individual users in the channel
+	# they have a mask of zero, since it isn't a toggleable setting
+	'v' : 0, # voiced 
+	'h' : 0, # half operator
+	'o' : 0, # operators
+	'a' : 0, # administrator
+	'q' : 0  # owner
 }
 
+# user mode symbols and masks
 user_modes = {
 	# "freely settable" modes
 	'i' : 2**0, # invisible
