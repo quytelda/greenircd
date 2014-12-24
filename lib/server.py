ff9b3565996bc44ead7c02e76370970557e394fe
Quytelda Gaiwin <admin@tamalin.org>
2014-12-24 15:09:30 -0500

    Finish module loading function

--- lib/server.py
+++ lib/server.py
@@ -2,6 +2,7 @@ import importlib
 
 import lib.module
 from lib.channel import Channel
+import mod
 
 MODULE_PKG = 'mod'
 
@@ -78,9 +79,9 @@ class Server(object):
 		command = _parse_message(message)
 
 		if id in self.clients:
-			print "%s (client): %s" % (id, message)
+			print("%s (client): %s" % (id, message))
 		elif id in self.servers:
-			print "%s (server): %s" % (id, message)
+			print("%s (server): %s" % (id, message))
 		else:
 			print("%s (unreg): %s" % (id, message))
 
@@ -94,16 +95,37 @@ class Server(object):
 
 	def __load_module(self, name):
 
+		HANDLER_PREFIX = "handle_"
+
 		try:
 			import inspect
 
 			module = importlib.import_module('.' + name, MODULE_PKG)
+			mod_objects = [x[1] \
+						   for x in inspect.getmembers(module, inspect.isclass) \
+						   if issubclass(x[1], lib.module.Module)]
 
-			objs = \
-				   [x[1] for x in inspect.getmembers(module, inspect.isclass) \
-					if (x[0] is not 'Module' and issubclass(x[1], lib.module.Module))]
+			# collect handlers from each module
+			handlers = []
+			for obj in mod_objects:
+				handlers += [entry \
+							 for entry in inspect.getmembers(obj, inspect.isfunction) \
+							 if entry[0].startswith(HANDLER_PREFIX)]
 
-			#TODO: set up loaded module objects
+			# construct handler dict
+			for handler in handlers:
+				ident = handler[0].split('_')
+
+				# ignore incorrectly named functions
+				# TODO: check for valid types
+				if len(ident) != 3: continue
+
+				type = ident[1]
+				handle = ident[2]
+
+				if type not in self.__command_handlers: continue
+
+				self.__command_handlers[type][handle] = handler[1]
 
 			print("* Loaded module: %s" % name)
 		except ImportError:
