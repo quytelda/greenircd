911fc9b9e25a3f9c87cca62254ae67e7989e56f5
Quytelda Kahja <quytelda@tamalin.org>
2015-03-20 17:26:36 -0500

    Changed messaging in PRIVMSG

--- mod/privmsg.py
+++ mod/privmsg.py
@@ -3,12 +3,14 @@ from lib.module import Module
 
 from lib.error import NameInUseError
 
+from lib.error import NoSuchTargetError
+
 class NickMod(Module):
 
 	command = "PRIVMSG"
 
 	def handle_unreg(self, source, message):
-		source.numeric(self.server.name, numeric.ERR_NOTREGISTERED, nick, ":Not registered.")
+		source.numeric(self.server.name, numeric.ERR_NOTREGISTERED, "*", ":Not registered.")
 
 	def handle_client(self, source, message):
 
@@ -18,4 +20,7 @@ class NickMod(Module):
 		target = message['params'][0]
 		msg = message['params'][1]
 
-		self.server.message_client(target, source, "PRIVMSG %s :%s" % (target, msg))
+		try:
+			self.server.message_client(target, source, "PRIVMSG %s :%s" % (target, msg))
+		except NoSuchTargetError as err:
+			self.server.numeric_message_client(self.server.name, numeric.ERR_NOSUCHNICK, source, ":No such nickname.")
